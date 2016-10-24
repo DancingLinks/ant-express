c328b913f83310a7cdeb7c615b97eecf7f4ed952
Yuan Yizhe <yuanyizhe@gmail.com>
2016-10-24 15:54:51 +0800

    add order token

--- model/order.js
+++ model/order.js
@@ -1,3 +1,5 @@
+var md5 = require('md5')
+var uuid = require('uuid')
 var mongoose = require('mongoose')
 var paginate = require('mongoose-paginate')
 var Schema = mongoose.Schema
@@ -7,6 +9,7 @@ var orderSchema = new Schema({
   parcel: {type: Schema.Types.ObjectId, ref: 'Parcel', required: true},
   parcelPath: {type: Schema.Types.ObjectId, ref: 'ParcelPath', required: true},
   status: {type: String, required: true, enum: ['confirm', 'passing', 'finish']},
+  token: {type: String, default: md5(uuid.v4())},
   credit: Number
 }, {timestamps: true})
 orderSchema.plugin(paginate)
