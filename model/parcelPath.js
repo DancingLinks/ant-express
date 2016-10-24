f6960ab197a80a0f24593dc51eaf103d2be2407f
Yuan Yizhe <yuanyizhe@gmail.com>
2016-10-24 15:55:08 +0800

    fix parcel path

--- model/parcelPath.js
+++ model/parcelPath.js
@@ -2,12 +2,12 @@ var mongoose = require('mongoose')
 var paginate = require('mongoose-paginate')
 var Schema = mongoose.Schema
 
-var parcelSchema = new Schema({
+var parcelPathSchema = new Schema({
   parcel: {type: Schema.Types.ObjectId, ref: 'Parcel'},
   start: {type: String, required: true},
   end: {type: String, required: true},
   status: {type: String, required: true, default: 'wait', enum: ['wait', 'confirm', 'finish']}
 }, {timestamps: true})
-parcelSchema.plugin(paginate)
+parcelPathSchema.plugin(paginate)
 
-module.exports = mongoose.model('ParcelPath', parcelSchema)
+module.exports = mongoose.model('ParcelPath', parcelPathSchema)
